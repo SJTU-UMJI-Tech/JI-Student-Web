acedeb3838931dd9ae8a916f27a12413fbbb4e49
tc-imba <liuyh615@sjtu.edu.cn>
2016-10-04 14:22:54 +0800

    vote for ylm scoreboard

--- application/views/secret/vote.php
+++ application/views/secret/vote.php
@@ -28,6 +28,9 @@
 		
 		该公司十分关心残疾人家庭子女的教育培养情况，每逢其子女中高考试，公司给其家长带薪陪考假，每年给予学费补助。在市慈善总会设立黎明慈善助学金，先后资助500名寒门学子完成大学学业计划。2014年开始，俞黎明同志分别在吉林大学、上海交通大学密西根学院各捐助500万元，助品学兼优的学生向更高层次发展，鼓励莘莘学子努力学习，他先后结对扶贫，资助社会生活困难残疾人、捐助老年协会、帮助癌症者康复、抗震赈灾、脑瘫儿康复中心、赞助母校计算机室及在上海交大设立学生中心等。历年来，俞黎明同志为慈善事业捐助累计2000余万元。被舟山市慈善总会授予“慈善献爱心荣誉单位”，“浙江慈善奖”（机构奖），曾获浙江省和国务院授予“扶残助残先进集体”称号；市“优秀福利企业”，“全国福利企业示范单位”，2016年6月被评为浙江省优秀共产党员。</p>
 	<hr>
+	<h5>Scoreboard</h5>
+	<div id="list"></div>
+	<hr>
 	<h5>Powered by <a href="https://github.com/tc-imba">tc-imba</a></h5>
 </div>
 
@@ -37,17 +40,48 @@
 <script type="text/javascript">
 	$(document).ready(function ()
 	{
-		$.ajax({
-			type: "get",
-			url: "http://vip.zjqq.mobi/index.php/vote/voteList.html?id=2346&page=5&num=5&_=" + (new Date()).getTime(),
-			dataType: "jsonp",
-			success: function (data)
-			{
-				console.log(data);
-				$("#score").html('COUNT : '+data.list[4].vote.score);
-			}
-		});
-		
+		var list = [];
+		var num = 0;
+		for (var i = 1; i <= 9; i++)
+		{
+			var url = "http://vip.zjqq.mobi/index.php/vote/voteList.html?id=2346&page=" + i + "&num=5&_=" + (new Date()).getTime();
+			$.ajax({
+				type: "get",
+				url: url,
+				dataType: "jsonp",
+				success: function (data)
+				{
+					//console.log(data);
+					for (var index in data.list)
+					{
+						var obj = data.list[index];
+						list.push({name: obj.user.name, score: obj.vote.score});
+						if(obj.voteid=='45229')
+						{
+							$("#score").html('COUNT : ' + data.list[4].vote.score);
+						}
+					}
+					if (++num >= 9)
+					{
+						list.sort(function (a, b)
+						{
+							return Number(b.score) - Number(a.score);
+						});
+						console.log(list);
+						for (var j in list)
+						{
+							var html = '<div class="row">' +
+							           '<div class="col-xs-2">' + (Number(j) + 1) + '</div>' +
+							           '<div class="col-xs-7">' + list[j].name + '</div>' +
+							           '<div class="col-xs-3">' + list[j].score + '</div>' +
+							           '</div>';
+							$("#list").append(html);
+						}
+						
+					}
+				}
+			});
+		}
 		
 		$("#btn").click(function ()
 		{
