862dd1030187e064f61f279bfe209cda184af0e3
tc-imba <liuyh615@sjtu.edu.cn>
2017-01-12 01:37:59 +0800

    homepage

---
+++ views/common/header.php
@@ -0,0 +1,36 @@
+<!--
+*
+*  JI-LIFE
+*  Powered by INSPINIA - Responsive Admin Theme
+*  https://github.com/SJTU-UMJI-Tech/JI-Student-Web
+*
+-->
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <!-- If page name is not defined, use a default one -->
+    <title><?php echo isset($page_name) ? $page_name : 'UM-SJTU JI LIFE'; ?></title>
+    
+    <link rel="shortcut icon" href="<?php echo ROOT_DIR; ?>/images/favicon.png">
+    
+    <!-- Bootstrap and Font Awesome Stylesheet-->
+    <link href="<?php echo ROOT_DIR; ?>/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo ROOT_DIR; ?>/font-awesome/css/font-awesome.css" rel="stylesheet">
+    
+    <!-- Toastr Stylesheet -->
+    <link href="<?php echo ROOT_DIR; ?>/css/plugins/toastr/toastr.min.css" rel="stylesheet">
+    
+    <!-- Gritter Stylesheet -->
+    <link href="<?php echo ROOT_DIR; ?>/js/plugins/gritter/jquery.gritter.css" rel="stylesheet">
+    
+    <!-- Inspinia Stylesheet -->
+    <link href="<?php echo ROOT_DIR; ?>/css/animate.css" rel="stylesheet">
+    <link href="<?php echo ROOT_DIR; ?>/css/style.css" rel="stylesheet">
+
+</head>
