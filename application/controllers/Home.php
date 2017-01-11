12bf913110a0d0257a99462bb91208064653a057
tc-imba <liuyh615@sjtu.edu.cn>
2017-01-12 01:37:59 +0800

    homepage

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,19 +2,21 @@
 
 class Home extends Front_Controller
 {
-	protected function redirect()
-	{
-		$this->__redirect();
-	}
-	
-	public function index()
-	{
-		$data['page_name'] = 'UM-SJTU JI Online';
-		
-		$obj = $this->Site_model->get_object('jbxx', 'User_obj', array('USER_ID' => '515370910207'));
-		$this->load->view('home', $data);
-		
-	}
-	
-	
+    protected function redirect()
+    {
+        $this->__redirect();
+    }
+    
+    public function index()
+    {
+        $data['page_name'] = 'UM-SJTU JI LIFE';
+        
+        $obj = $this->Site_model->get_object('jbxx', 'User_obj', array('USER_ID' => '515370910207'));
+        $this->load->view('home', $data);
+        $_SESSION['user_id'] = '515370910207';
+        $_SESSION['user_name'] = 'Liu Yihao';
+        $_SESSION['user_type'] = 'Manager';
+    }
+    
+    
 }
