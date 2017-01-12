ae5e680a9bba29a9e0c3e2020cd1dbb9414cc6ab
tc-imba <liuyh615@sjtu.edu.cn>
2017-01-13 00:56:09 +0800

    navbar

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -11,6 +11,12 @@ class Home extends Front_Controller
     {
         $data['page_name'] = 'UM-SJTU JI LIFE';
         $obj = $this->Site_model->get_object('jbxx', 'User_obj', array('USER_ID' => '515370910207'));
+        $this->load->model('Navbar_model');
+        $nav = $this->Navbar_model->get_navbar_data();
+        $str = $this->Navbar_model->generate_navbar($nav);
+        //print_r($nav);
+        //echo $str;
+        $data['nav'] = $str;
         $this->load->view('home', $data);
     }
     
