7e46b52c17fd071383af35ed49b78062ab4d6654
tc-imba <liuyh615@126.com>
2017-01-27 23:51:22 +0800

    ji-list-view

--- js/ji/ji-list-view.js
+++ js/ji/ji-list-view.js
@@ -15,14 +15,14 @@
         factory(jQuery);
     }
 })(function ($, Handlebars) {
-    
+
     'use strict';
-    
+
     function JIListView($element, options) {
         var i;
-        
+
         this.$container = $element;
-        
+
         if (options.templates) {
             if (options.templates.list) {
                 Handlebars.registerPartial('list', options.templates.list);
@@ -41,25 +41,26 @@
                 this.itemTemplate = Handlebars.compile(options.templates.item);
             } else return;
         }
-        
+
         this.$list = this.$container.find("tbody");
-        
+        this.processRow = options.processRow;
         this.listData = {};
-        
+        this.listNum = 0;
+
         this.expand();
-        
-        
+
+
     }
-    
+
     JIListView.prototype = {
-        
+
         constructor: JIListView,
-        
+
         expand: function () {
             var _this = this;
-            
+
             this.$list.find(".btn-expand").parentsUntil("tr").remove();
-            
+
             $.ajax({
                 type: 'GET',
                 url: window.ROOT_DIR + '/scholarships/ajax_search',
@@ -68,8 +69,8 @@
                     key: 'all',
                     keywords: '',
                     order: 'Newest',
-                    limit: 5,
-                    offset: 0
+                    limit: 1,
+                    offset: this.listNum
                 },
                 dataType: 'json',
                 success: function (data) {
@@ -79,40 +80,51 @@
                     console.log('fail')
                 }
             });
-            
+
         },
-        
+
         addItem: function (data) {
             var newFlag = false;
             for (var i = 0; i < data.length; i++) {
-                var row = data[i];
+                var row = this.processRow(data[i]);
+
                 if (this.listData.hasOwnProperty(row.id)) {
                     // Update
-                    
+                    var $old = this.$list.find("tr[data-id=" + row.id + "]");
+                    $old.html(this.itemTemplate(row));
+                    //console.log($old);
                 } else {
                     // Create
-                    this.listData[data.id] = row;
+                    this.listData[row.id] = row;
+                    this.listNum++;
                     this.$list.append(this.itemTemplate(row));
-                    console.log(row);
+                    var $new = this.$list.find("tr[data-id=" + row.id + "]");
+
+                    $new.on('click', $.proxy(this.itemCollapse, this));
+
+                    //console.log(row);
                     newFlag = true;
                 }
-                
             }
-            
+
             if (!newFlag) {
-            
+
             }
             else {
                 this.$list.append('<tr><td colspan="5" class="text-center"><a class="btn-link btn-expand"><i class="fa fa-angle-double-down" aria-hidden="true"></i></a></td></tr>');
                 this.$list.find(".btn-expand").on('click', $.proxy(this.expand, this));
             }
+        },
+
+        itemCollapse: function (e) {
+            console.log(1);
         }
-        
-        
+
+
     };
-    
+
     $.fn.jiListView = function (option) {
         return new JIListView(this, option);
     };
-    
+
 });
--- views/scholarships/list.php
+++ views/scholarships/list.php
@@ -13,18 +13,35 @@
 <?php include 'templates/list-item.hbs'; ?>
 
 <script type="text/javascript">
-    
+
     window.ROOT_DIR = '';
-    
+
     require(['jquery', 'ji-list-view'], function ($) {
         $(document).ready(function () {
-            
+
             $("#body-wrapper").jiListView({
                 title: "Scholarships",
                 templates: {
                     "ibox": $("#ji-ibox-template").html(),
                     "list": $("#list-template").html(),
                     "item": $("#list-item-template").html()
+                },
+                processRow: function (row) {
+                    var start = new Date(row.start_date).getTime();
+                    var end = new Date(row.end_date).getTime();
+                    var now = new Date().getTime();
+                    var percent = Math.round((now - start) / (end - start) * 100.);
+                    percent = Math.max(0, Math.min(100, percent));
+                    if (percent === 100) row.time_after = true;
+                    
+                    row.time_percent = percent;
+
+                    percent = Math.round(row.current_num / row.max_num * 100.);
+                    percent = Math.max(0, Math.min(100, percent));
+                    row.people_percent = percent;
+                    if (percent === 100) row.people_full = true;
+
+                    return row;
                 }
             });
         });
