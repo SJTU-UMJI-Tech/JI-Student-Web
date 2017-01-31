744824ccb67850b8cbb12a461afc537526e25047
sunyi <grasshopper001@sjtu.edu.cn>
2017-02-01 00:49:08 +0800

    gpa/terms

---
+++ deployment/build.js
@@ -0,0 +1,41 @@
+'use strict';
+const fs=require('fs');
+const shelljs=require('shelljs');
+const crypto=require('crypto');
+function process_dir(dir){
+	const files=fs.readdirSync(dir);
+	for(let i=0;i<files.length;i++){
+		let file_dir=dir+'/'+files[i];
+		const stats=fs.statSync(file_dir);
+		if(stats.isDirectory()){
+			process_dir(file_dir);
+		}
+		else{
+			let check_min_js=files[i].match (/\.min[\W\w]*\.js/);
+			let check_js=files[i].match (/\.js$/);
+			if(!check_min_js&&check_js)
+			{
+				let filename=file_dir.replace (/\.js$/,'');
+				process_js(filename);
+				break;
+			}
+		}
+	}
+}
+
+function process_js(filename){
+	const buffer=fs.readFileSync(filename+'.js');
+	const hash=crypto.createHash('md5');
+	hash.update(buffer);
+	let signature=hash.digest('hex').substring(0,6);
+	console.log(signature);
+	try{
+		fs.access(filename+'.min.'+signature+'.js')
+	}catch(err){
+		console.log('not found');
+	}
+	}
+	
+
+
+process_dir('../js/ji');
