ccb80da951320f67dd354faf9ed7f0d135fc1c94
tc-imba <liuyh615@sjtu.edu.cn>
2017-02-02 00:09:53 +0800

    bugs

--- deployment/build.test.js
+++ deployment/build.test.js
@@ -2,6 +2,6 @@
  * Created by sunyi on 2017/2/1.
  */
 const builder = require('./requirejs-builder');
-var test=builder.initBuilder({});
+let test      = builder.initBuilder({});
 
-test.addAppDir(process.cwd ()+'/js/ji');
+test.addAppDir('./js/ji','./js-dist/ji');
--- deployment/requirejs-builder.js
+++ deployment/requirejs-builder.js
@@ -8,40 +8,32 @@ const crypto  = require('crypto');
 const hash    = crypto.createHash('md5');
 
 class RequireJSBuilder {
-	constructor(options)
-	{
-		
-	}
-		
-		
-		addAppDir(inputDir, outputDir)
-		{
-			let traversal = (relativeDir)=>
-			{
-				const files = fs.readFileSync(inputDir + relativeDir);
-				for (let i = 0; i < files.length; i++)
-				{
-					let file_dir = inputDir+relativeDir + '/' + files[i];
-					const stats = fs.statSync(file_dir);
-					if (stats.isDirectory(file_dir))
-					{
-						traversal(file_dir);
-					}
-					else
-					{
-						let check_min_js = files[i].match(/\.min[\W\w]*\.js/);
-						let check_js = files[i].match(/\.js$/);
-						if (!check_min_js && check_js)
-						{
-							relativeDir = file_dir.replace(/\.js$/, '');
-							console.log(outputDir+relativeDir);
-						}
-					}
-				}
-			}
-			traversal('');
-		}
-	
+    constructor(options) {
+    
+    }
+    
+    addAppDir(inputDir, outputDir) {
+        let traversal = (relativeDir) => {
+            const files = fs.readdirSync(inputDir + relativeDir);
+            for (let i = 0; i < files.length; i++) {
+                let file_dir = relativeDir + '/' + files[i];
+                const stats  = fs.statSync(inputDir + file_dir);
+                if (stats.isDirectory()) {
+                    traversal(file_dir);
+                }
+                else {
+                    let check_min_js = files[i].match(/\.min[\W\w]*\.js/);
+                    let check_js     = files[i].match(/\.js$/);
+                    if (!check_min_js && check_js) {
+                        file_dir = file_dir.replace(/\.js$/, '');
+                        console.log(outputDir + file_dir);
+                    }
+                }
+            }
+        };
+        traversal('');
+    }
+    
 }
 
 
