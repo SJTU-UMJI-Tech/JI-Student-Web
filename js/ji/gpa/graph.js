f9cd128f0e9ec827a0541eff92b0f7be9be70770
tc-imba <liuyh615@sjtu.edu.cn>
2017-02-11 12:45:30 +0800

    babel

--- js/ji/gpa/graph.js
+++ js/ji/gpa/graph.js
@@ -4,27 +4,23 @@
 ;define([
     'require', 'exports', 'module',
     'jquery', 'handlebars.runtime', 'footable', 'chartjs',
-    'templates/common/ibox', 'templates/gpa/graph'
+    'templates/common/ibox', 'templates/common/modal', 'templates/gpa/graph'
 ], function (require, exports, module) {
     
-    var $ = require('jquery');
-    var Handlebars = require('handlebars.runtime');
+    const $          = require('jquery'),
+          Handlebars = require('handlebars.runtime');
     
-    module.exports = function (options) {
+    module.exports = (options) => {
         
-        var i, j;
-        
-        var courses = {"course": true};
-        courses = options.courses;
-        for (i in courses.course) {
+        let courses = options.courses;
+        for (let i in courses.course) {
             if (courses.course.hasOwnProperty(i)) courses.course[i].id = i;
         }
         
-        var score = [{course_id: true, grade: true}];
-        score = options.score;
-        for (i = 0; i < score.length; i++) {
+        let score = options.score;
+        for (let i = 0; i < score.length; i++) {
             if (courses.course.hasOwnProperty(score[i].course_id)) {
-                var course = courses.course[score[i].course_id];
+                let course = courses.course[score[i].course_id];
                 if (score[i].grade == 0) {
                     course.fail = true;
                 } else if (score[i].grade > 0) {
@@ -33,7 +29,7 @@
                     course.select = true;
                 }
                 if (course.hasOwnProperty('equivalent')) {
-                    for (j = 0; j < course.equivalent.length; j++) {
+                    for (let j = 0; j < course.equivalent.length; j++) {
                         if (courses.course.hasOwnProperty(course.equivalent[j])) {
                             courses.course[course.equivalent[j]].equal = true;
                         }
@@ -42,23 +38,47 @@
             }
         }
         
-        var template = require('templates/common/ibox');
+        let template = require('templates/common/ibox');
         Handlebars.registerPartial('graph', require('templates/gpa/graph'));
         
         // Main Table
-        var config = {
-            "id": "graph-table",
+        let config = {
+            "id"   : "graph-table",
             "title": "Graphs",
-            "body": [{
+            "body" : [{
                 "template": "graph",
-                "data": {courses: courses.course}
+                "data"    : {courses: courses.course}
             }]
         };
-        $("#body-wrapper").prepend(template(config));
+        $("#body-wrapper").append(template(config));
+        
+        
+        // The modal of two graphs
+        template = require('templates/common/modal');
+        config   = {
+            "id"    : "modal-agree",
+            "header": {"title": "Confirmation"},
+            "body"  : [{
+                "html": "<h3><strong>I've read the terms of service and I agreed about all of the items.</strong></h3>"
+            }],
+            "footer": [{
+                "button": {
+                    "id"  : "modal-agree-btn-close", "text": "Close",
+                    "type": "white", "close": true
+                }
+            }, {
+                "button": {
+                    "id"  : "modal-agree-btn-confirm", "text": "Confirm",
+                    "type": "primary", "href": "<?php echo base_url('GPA/terms?confirm=1');?>"
+                }
+            }]
+        };
+        $("#body-wrapper").append(template(config));
+        
         
         $(".footable").footable();
         
-        $(".footable").find(".pagination").on('click', function () {
+        $(".footable").find(".pagination").on('click', () => {
             window.location.hash = '';
             window.location.hash = 'body-wrapper';
         });
@@ -70,41 +90,39 @@
             '43': 0, '40': 1, '37': 2, '33': 3, '30': 4, '27': 5,
             '23': 6, '20': 7, '17': 8, '10': 9, '0': 10
         };
-        const grade_list = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D', 'F'];
-        const color_list = [
+        const grade_list          = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D', 'F'];
+        const color_list          = [
             '#7AA7E1', '#5ADED8', '#5CCFA5', '#39E13B', '#77E17A', '#7FE17C',
             '#BBE162', '#DEB367', '#E16158', '#E1487A', '#DE2027'];
         
-        $("#graph-table").find(".tr-course").on('click', function (e) {
+        $("#graph-table").find(".tr-course").on('click', (e) => {
             if ($(this).data('graph')) {
-                var $graph = $(this).next();
+                let $graph = $(this).next();
                 $graph.find(".row").collapse('hide');
-                setTimeout(function () {
-                    $graph.remove();
-                }, 200);
+                setTimeout(() => $graph.remove(), 200);
                 $(this).data('graph', false);
                 return;
             }
-            var id = $(this).children("[type=id]").data('id');
-            var _this = $(this);
+            const id    = $(this).children("[type=id]").data('id');
+            const _this = $(this);
             $.ajax({
-                url: window.ROOT_DIR + '/GPA/graph_score',
-                type: 'GET',
+                url     : window.ROOT_DIR + '/GPA/graph_score',
+                type    : 'GET',
                 dataType: 'json',
-                data: {id: id},
-                success: function (data) {
+                data    : {id: id},
+                success : (data) => {
                     generate_graph(data, _this);
                 },
-                error: function (data) {
-                    console.log('fail');
+                error   : (data) => {
+                    window.console.log('fail', data);
                 }
             });
         });
         
-        function generate_graph(score_list, $target) {
+        const generate_graph = (score_list, $target) => {
             $target.data('graph', true);
             
-            var html;
+            let html;
             if (score_list.length === 0) {
                 html = ['<div class="row collapsed">',
                     'Sorry, we don\'t have data for this course.',
@@ -123,99 +141,71 @@
                     '</div>'].join('');
             }
             $target.after('<tr>111<td colspan="5" class="text-center td-graph">' + html + '<td></tr>');
-            var $graph = $target.next();
+            const $graph = $target.next();
             
             if (score_list.length > 0) {
-                var display = {
+                let display = {
                     grade: [], data: [], color: [], data_all: []
                 };
-                for (i = 0; i < grade_list.length; i++) {
+                for (let i = 0; i < grade_list.length; i++) {
                     display.data_all.push(0);
                 }
-                for (i = 0; i < score_list.length; i++) {
+                for (let i = 0; i < score_list.length; i++) {
                     if (score_to_index_list.hasOwnProperty(score_list[i].grade)) {
-                        var index = score_to_index_list[score_list[i].grade];
+                        const index = score_to_index_list[score_list[i].grade];
                         display.grade.push(grade_list[index]);
                         display.data.push(score_list[i]['count(grade)']);
                         display.data_all[index] = score_list[i]['count(grade)'];
                         display.color.push(color_list[index]);
                     }
                 }
-                //console.log(display.data_all);
                 
-                // Graph Line
-                /*config = {
-                 "id": "graph-line-ibox",
-                 "title": "Line Graph - ",
-                 "body": [{
-                 "html": '<div><canvas id="graph-line" height="140"></canvas></div>'
-                 }]
-                 };
-                 $graph.find(".graph-line-wrapper").html(template(config));*/
-                
-                var lineData = {
-                    labels: grade_list,
+                const lineData    = {
+                    labels  : grade_list,
                     datasets: [
                         {
-                            label: "Number",
-                            backgroundColor: 'rgba(26,179,148,0.5)',
-                            borderColor: "rgba(26,179,148,0.7)",
+                            label               : "Number",
+                            backgroundColor     : 'rgba(26,179,148,0.5)',
+                            borderColor         : "rgba(26,179,148,0.7)",
                             pointBackgroundColor: "rgba(26,179,148,1)",
-                            pointBorderColor: "#fff",
-                            data: display.data_all
+                            pointBorderColor    : "#fff",
+                            data                : display.data_all
                         }
                     ]
                 };
-                
-                var max = Math.ceil(Math.max.apply(null, display.data) / 5) * 5;
-                
-                var lineOptions = {
+                const max         = Math.ceil(Math.max.apply(null, display.data) / 5) * 5;
+                const lineOptions = {
                     responsive: true,
-                    legend: {display: false},
-                    scales: {
+                    legend    : {display: false},
+                    scales    : {
                         yAxes: [{
                             ticks: {
-                                max: max,
-                                min: 0,
+                                max     : max,
+                                min     : 0,
                                 stepSize: max / 5
                             }
                         }]
                     }
                 };
-                
-                var graphElement = $graph.find(".graph-line").first();
-                var ctx = graphElement[0].getContext("2d");
-                //var ctx = document.getElementById("graph-line").getContext("2d");
+                let graphElement  = $graph.find(".graph-line").first();
+                let ctx           = graphElement[0].getContext("2d");
                 new Chart(ctx, {type: 'line', data: lineData, options: lineOptions});
                 
-                // Graph Pie
-                /*config = {
-                 "id": "graph-pie-ibox",
-                 "title": "Pie Graph - ",
-                 "body": [{
-                 "html": '<div><canvas id="graph-pie" height="140"></canvas></div>'
-                 }]
-                 };
-                 $graph.find(".graph-pie-wrapper").html(template(config));*/
-                
-                
-                var doughnutData = {
-                    labels: display.grade,
+                const doughnutData    = {
+                    labels  : display.grade,
                     datasets: [{
-                        data: display.data,
+                        data           : display.data,
                         backgroundColor: display.color
                     }]
                 };
-                var doughnutOptions = {
+                const doughnutOptions = {
                     responsive: true
                 };
-                graphElement = $graph.find(".graph-pie").first();
-                ctx = graphElement[0].getContext("2d");
-                //var ctx4 = document.getElementById("graph-pie").getContext("2d");
+                graphElement          = $graph.find(".graph-pie").first();
+                ctx                   = graphElement[0].getContext("2d");
                 new Chart(ctx, {type: 'doughnut', data: doughnutData, options: doughnutOptions});
             }
             $graph.find(".row").collapse();
-            
         }
         
     }
