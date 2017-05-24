b0036f711902c36d2b92cffd6e9f087cf4374253
tc-imba <liuyh615@sjtu.edu.cn>
2017-05-25 02:48:40 +0800

    weui

---
+++ application/models/Career_model.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Class Career_model
+ *
+ * @category   ji-life
+ * @package    ji-life
+ * @author     tc-imba
+ * @copyright  2016-2017 umji-sjtu
+ */
+class Career_model extends CI_Model
+{
+    const TABLE   = 'career';
+    //const LIBRARY = 'Scholarships_obj';
+    
+    /**
+     * Career_model constructor.
+     */
+    function __construct()
+    {
+        parent::__construct();
+        //$this->load->library($this::LIBRARY);
+    }
+    
+    
+    
+}
+
---
+++ views/common/weui.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <!-- If page name is not defined, use a default one -->
+    <title><?php echo isset($page_name) ? $page_name : 'UM-SJTU JI LIFE'; ?></title>
+    
+    <link rel="shortcut icon" href="<?php echo ROOT_DIR; ?>/img/favicon.png">
+    <link rel="stylesheet" href="<?php echo ROOT_DIR; ?>/node_modules/weui/dist/style/weui.min.css">
+    
+    <style>
+        html, body{
+            height: 100%
+        }
+    </style>
+
+</head>
+
+<body>
+
+<script src="<?php echo ROOT_DIR; ?>/node_modules/weui.js/dist/weui.min.js"></script>
+
+</body>
+</html>
